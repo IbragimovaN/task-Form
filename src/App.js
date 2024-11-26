32c30de64471bccc95f7c24390ea867e01bd7f3d
Anastasiya <anastasiya.ibr@mail.ru>
2024-11-26 17:40:19 +0300

    feat: компонент app основа

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import { Signin } from "./components/Signin/Signin";
+import { Signup } from "./components/Signup/Signup";
+import { Button } from "./components/Button/Button";
+import styles from "./App.module.css";
+
 function App() {
-  return <div>app</div>;
+  const [isOpenRegForm, setIsOpenRegForm] = useState(false);
+
+  return (
+    <div className={styles.app}>
+      <div>
+        <Button
+          onClick={() => setIsOpenRegForm(false)}
+          classname={styles.appBtn}
+        >
+          {" "}
+          Войти
+        </Button>{" "}
+        /
+        <Button
+          onClick={() => setIsOpenRegForm(true)}
+          classname={styles.appBtn}
+        >
+          Зарегистрироваться
+        </Button>
+      </div>
+
+      {isOpenRegForm ? <Signup /> : <Signin />}
+    </div>
+  );
 }
 
 export default App;
