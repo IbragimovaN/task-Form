64708e6ca4b9112bbb250270b12087da927edd66
Anastasiya <anastasiya.ibr@mail.ru>
2024-11-29 16:12:05 +0300

    fix: убрала лишний useEffect который  испрользовался для поиска ошибки

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -4,13 +4,6 @@ import { CreateInputField } from "../CreateInputField/CreateInputField";
 export const Signin = ({ handleSubmit, inputsArr, setInputsArr }) => {
   const [data, setData] = useState({});
 
-  useEffect(() => {
-    console.log("IN");
-    return () => {
-      console.log("IN отписка");
-    };
-  }, []);
-
   return (
     <CreateInputField
       handleSubmit={handleSubmit}
